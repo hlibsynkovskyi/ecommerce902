4030d717c5a1f82cd1754843a1c5448129d2e5d0
Hlib Synkovskyi <gleb.sinkovskiy@gmail.com>
2019-05-17 20:24:40 +0300

    liipimage

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -74,189 +74,189 @@ class Product
 	 */
 	private $updatedAt;
 
-    /**
-     * @ORM\Column(type="string", length=255, nullable=true)
-     */
-    private $imageOriginalName;
+	/**
+	 * @ORM\Column(type="string", length=255, nullable=true)
+	 */
+	private $imageOriginalName;
 
 	public function __construct()
-         	{
-         		$this->isTop = false;
-         		$this->categories = new ArrayCollection();
-         		$this->orderItems = new ArrayCollection();
-         	}
+	{
+		$this->isTop = false;
+		$this->categories = new ArrayCollection();
+		$this->orderItems = new ArrayCollection();
+	}
 
 	public function getId(): ?int
-         	{
-         		return $this->id;
-         	}
+	{
+		return $this->id;
+	}
 
 	public function getName(): ?string
-         	{
-         		return $this->name;
-         	}
+	{
+		return $this->name;
+	}
 
 	public function setName(string $name): self
-         	{
-         		$this->name = $name;
+	{
+		$this->name = $name;
 
-         		return $this;
-         	}
+		return $this;
+	}
 
 	public function getDescription(): ?string
-         	{
-         		return $this->description;
-         	}
+	{
+		return $this->description;
+	}
 
 	public function setDescription(?string $description): self
-         	{
-         		$this->description = $description;
+	{
+		$this->description = $description;
 
-         		return $this;
-         	}
+		return $this;
+	}
 
 	public function getPrice(): ?int
-         	{
-         		return $this->price;
-         	}
+	{
+		return $this->price;
+	}
 
 	public function setPrice(int $price): self
-         	{
-         		$this->price = $price;
+	{
+		$this->price = $price;
 
-         		return $this;
-         	}
+		return $this;
+	}
 
 	public function getCount(): ?int
-         	{
-         		return $this->count;
-         	}
+	{
+		return $this->count;
+	}
 
 	public function setCount(?int $count): self
-         	{
-         		$this->count = $count;
+	{
+		$this->count = $count;
 
-         		return $this;
-         	}
+		return $this;
+	}
 
 	public function getIsTop(): ?bool
-         	{
-         		return $this->isTop;
-         	}
+	{
+		return $this->isTop;
+	}
 
 	public function setIsTop(bool $isTop): self
-         	{
-         		$this->isTop = $isTop;
+	{
+		$this->isTop = $isTop;
 
-         		return $this;
-         	}
+		return $this;
+	}
 
 	/**
 	 * @return Collection|Category[]
 	 */
 	public function getCategories(): Collection
-         	{
-         		return $this->categories;
-         	}
+	{
+		return $this->categories;
+	}
 
 	public function addCategory(Category $category): self
-         	{
-         		if ( !$this->categories->contains($category) ) {
-         			$this->categories[] = $category;
-         		}
+	{
+		if ( !$this->categories->contains($category) ) {
+			$this->categories[] = $category;
+		}
 
-         		return $this;
-         	}
+		return $this;
+	}
 
 	public function removeCategory(Category $category): self
-         	{
-         		if ( $this->categories->contains($category) ) {
-         			$this->categories->removeElement($category);
-         		}
+	{
+		if ( $this->categories->contains($category) ) {
+			$this->categories->removeElement($category);
+		}
 
-         		return $this;
-         	}
+		return $this;
+	}
 
 	/**
 	 * @return Collection|OrderItem[]
 	 */
 	public function getOrderItems(): Collection
-         	{
-         		return $this->orderItems;
-         	}
+	{
+		return $this->orderItems;
+	}
 
 	public function addOrderItem(OrderItem $orderItem): self
-         	{
-         		if ( !$this->orderItems->contains($orderItem) ) {
-         			$this->orderItems[] = $orderItem;
-         			$orderItem->setProduct($this);
-         		}
+	{
+		if ( !$this->orderItems->contains($orderItem) ) {
+			$this->orderItems[] = $orderItem;
+			$orderItem->setProduct($this);
+		}
 
-         		return $this;
-         	}
+		return $this;
+	}
 
 	public function removeOrderItem(OrderItem $orderItem): self
-         	{
-         		if ( $this->orderItems->contains($orderItem) ) {
-         			$this->orderItems->removeElement($orderItem);
-         			// set the owning side to null (unless already changed)
-         			if ( $orderItem->getProduct() === $this ) {
-         				$orderItem->setProduct(null);
-         			}
-         		}
+	{
+		if ( $this->orderItems->contains($orderItem) ) {
+			$this->orderItems->removeElement($orderItem);
+			// set the owning side to null (unless already changed)
+			if ( $orderItem->getProduct() === $this ) {
+				$orderItem->setProduct(null);
+			}
+		}
 
-         		return $this;
-         	}
+		return $this;
+	}
 
 	public function getImage(): ?File
-         	{
-         		return $this->image;
-         	}
+	{
+		return $this->image;
+	}
 
 	public function setImage(?File $image): Product
-         	{
-         		$this->image = $image;
+	{
+		$this->image = $image;
 
-         		if ($image !== null) {
-         			$this->updatedAt = new \DateTimeImmutable();
-         		}
+		if ( $image !== null ) {
+			$this->updatedAt = new \DateTimeImmutable();
+		}
 
-         		return $this;
-         	}
+		return $this;
+	}
 
 	public function getImageName(): ?string
-         	{
-         		return $this->imageName;
-         	}
+	{
+		return $this->imageName;
+	}
 
-	public function setImageName(string $imageName): self
-         	{
-         		$this->imageName = $imageName;
+	public function setImageName(?string $imageName): self
+	{
+		$this->imageName = $imageName;
 
-         		return $this;
-         	}
+		return $this;
+	}
 
 	public function getUpdatedAt(): ?\DateTimeInterface
-         	{
-         		return $this->updatedAt;
-         	}
+	{
+		return $this->updatedAt;
+	}
 
 	public function setUpdatedAt(?\DateTimeInterface $updatedAt): self
-         	{
-         		$this->updatedAt = $updatedAt;
+	{
+		$this->updatedAt = $updatedAt;
 
-         		return $this;
-         	}
+		return $this;
+	}
 
-    public function getImageOriginalName(): ?string
-    {
-        return $this->imageOriginalName;
-    }
+	public function getImageOriginalName(): ?string
+	{
+		return $this->imageOriginalName;
+	}
 
-    public function setImageOriginalName(?string $imageOriginalName): self
-    {
-        $this->imageOriginalName = $imageOriginalName;
+	public function setImageOriginalName(?string $imageOriginalName): self
+	{
+		$this->imageOriginalName = $imageOriginalName;
 
-        return $this;
-    }
+		return $this;
+	}
 }
